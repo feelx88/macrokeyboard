0dadb75c457d722abfc821d14495eb6afb648463
Felix Müller <feelx8@googlemail.com>
2021-08-24 22:23:20 +0200

    Made closing the main window not exiting the application (mainly for win).

--- qt/macrokeyboard/main.cpp
+++ qt/macrokeyboard/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    a.setQuitOnLastWindowClosed(false);
+
     a.setApplicationName("macrokeyboard");
     a.setOrganizationName("feelx88");
     a.setOrganizationDomain("feelx88.de");
